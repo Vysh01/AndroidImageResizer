a35ca3f5a5000014e8ba7b9385a7dc39fb0dcbea
vysh <vishal.shrestha9794@gmail.com>
2020-07-04 02:48:47 +0545

    Initial Commit

---
+++ ImageResizer.java
@@ -0,0 +1,40 @@
+import android.graphics.Bitmap;
+import android.util.Log;
+
+public class ImageResizer {
+
+	//For Image Size 640*480, use MAX_SIZE =  307200 as 640*480 307200
+    //private static long MAX_SIZE = 360000;
+    //private static long THUMB_SIZE = 6553;
+
+    public static Bitmap reduceBitmapSize(Bitmap bitmap, MAX_SIZE) {
+        double ratioSquare;
+        int bitmapHeight, bitmapWidth;
+        bitmapHeight = bitmap.getHeight();
+        bitmapWidth = bitmap.getWidth();
+        ratioSquare = (bitmapHeight * bitmapWidth) / MAX_SIZE;
+        if (ratioSquare <= 1)
+            return bitmap;
+        double ratio = Math.sqrt(ratioSquare);
+        Log.d("mylog", "Ratio: " + ratio);
+        int requiredHeight = (int) Math.round(bitmapHeight / ratio);
+        int requiredWidth = (int) Math.round(bitmapWidth / ratio);
+        return Bitmap.createScaledBitmap(bitmap, requiredWidth, requiredHeight, true);
+    }
+
+    public static Bitmap generateThumb(Bitmap bitmap) {
+        double ratioSquare;
+        int bitmapHeight, bitmapWidth;
+        bitmapHeight = bitmap.getHeight();
+        bitmapWidth = bitmap.getWidth();
+        ratioSquare = (bitmapHeight * bitmapWidth) / THUMB_SIZE;
+        if (ratioSquare <= 1)
+            return bitmap;
+        double ratio = Math.sqrt(ratioSquare);
+        Log.d("mylog", "Ratio: " + ratio);
+        int requiredHeight = (int) Math.round(bitmapHeight / ratio);
+        int requiredWidth = (int) Math.round(bitmapWidth / ratio);
+        return Bitmap.createScaledBitmap(bitmap, requiredWidth, requiredHeight, true);
+    }
+
+}
